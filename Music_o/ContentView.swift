085667627c3f30dad7ec99dbde7a738c3f11d629
Chungjaewoo <101718783+adfkov@users.noreply.github.com>
2024-10-29 23:42:00 +0900

    Initial Commit

---
+++ Music_o/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Music_o
+//
+//  Created by 정선우 on 10/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Music_o/Music_oApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_oApp.swift
+//  Music_o
+//
+//  Created by 정선우 on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Music_oApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
